8c7880bf247b61a6baab5da99575e6d19b0ebe3b
cc <576237121@qq.com>
2016-07-01 09:36:44 +0800

    环绕通知

--- spring-1/src/main/java/com/ccit/advice/MyAspect.java
+++ spring-1/src/main/java/com/ccit/advice/MyAspect.java
@@ -1,6 +1,8 @@
 package com.ccit.advice;
 
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class MyAspect {
     public void beforeAdvice(){
         System.out.println("这是前置通知");
@@ -13,5 +15,17 @@ public class MyAspect {
     }
     public void afterAdvice(){
         System.out.println("这是最终通知");
+    }
+    public void aroundAdvice(ProceedingJoinPoint pjp){
+        try {
+            System.out.println("这是前置通知");
+            Object o = pjp.proceed();
+            System.out.println("这是后置通知");
+        } catch (Throwable throwable) {
+            System.out.println("这是异常通知");
+            throwable.printStackTrace();
+        } finally {
+            System.out.println("这是最终通知");
+        }
     }
 }
