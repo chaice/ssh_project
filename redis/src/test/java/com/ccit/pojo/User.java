1061582b734dcaba3c718eed3c44baf3335a5f43
cc <576237121@qq.com>
2016-07-24 22:07:59 +0800

    创建用户

---
+++ redis/src/test/java/com/ccit/pojo/User.java
@@ -0,0 +1,43 @@
+package com.ccit.pojo;
+
+/**
+ * Created by cc on 2016/7/24.
+ */
+public class User {
+    private Integer id;
+    private String username;
+    private String name;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
